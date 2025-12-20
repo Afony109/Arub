84348b7dd991ec1e0597cc2519e32b36512f296b
Afony109 <alvlaf3@gmail.com>
2025-12-20 07:30:23 +0200

    Update contracts.js

--- js/contracts.js
+++ js/contracts.js
@@ -13,6 +13,17 @@ import { ERC20_ABI, ORACLE_ABI } from './abis.js';
 
 console.log('[CONTRACTS] contracts.js loaded, build:', Date.now());
 
+ // Core addresses
+  USDT_ADDRESS: '0xFd086bC7CD5C481DCC9C85ebE478A1C0b69FCbb9',
+  TOKEN_ADDRESS: '0x161296CD7F742220f727e1B4Ccc02cAEc71Ed2C6',
+
+  // New explicit keys (for future integration)
+  ANTIRUB_ADDRESS: '0x18D1d662371AD5732Ef996A054bd7672ab626368',
+  VAULT_ADDRESS: '0xE5dB8Bc105775EDa8A68B5B0aA17f91F492f102B',
+  PRESALE_ADDRESS: '0x44960eDa62860Fb54C143f742122619c25a129d1',
+  ORACLE_ADDRESS: '0xC15fFAA8D6835e3238c9B73428edb6A56cb3AF89',
+};
+
 // -----------------------------
 // State
 // -----------------------------
